e8dc083f0afb00e3b51e1365f3fcc5e07e6a2d57
Simone Lusenti <lusenti.s@gmail.com>
2011-12-30 12:36:03 +0100

    Fixed intents

--- htmlfallbackmiddleware/middleware.py
+++ htmlfallbackmiddleware/middleware.py
@@ -1,7 +1,7 @@
 """
 
-For each HTTP request, if no url can handle it, searches for a 
-suitable template (based on request uri) and renders it (similar 
+For each HTTP request, if no url can handle it, searches for a
+suitable template (based on request uri) and renders it (similar
 to flatpage.middleware.FlatpageFallbackMiddleware).
 
 """
@@ -15,26 +15,27 @@ class HTMLFallbackMiddleware(object):
     def process_response(self, request, response):
         if response.status_code != 404:
             return response
-        try:
-			path = request.path_info.lstrip('/') # remove leading /
-			
-			try:
-				# search for 'myapp/list.html'
-				if path.endswith('.html'):
-					return render_to_response(path)
-				else
-					return render_to_response(''.join([path, '.html']))
-			except:
-				# search for 'list.html'
-				(_, _, path_ending) = path.rpartition('/')
-				if path_ending.endswith('.html'):
-					return render_to_response(path_ending)
-				else
-					return render_to_response(''.join([path_ending, '.html']))
 
-		except TemplateDoesNotExist:
-			raise Http404
-		
+        try:
+            path = request.path_info.lstrip('/') # remove leading /
+
+            try:
+                # search for 'myapp/list.html'
+                if path.endswith('.html'):
+                    return render_to_response(path)
+                else
+                    return render_to_response(''.join([path, '.html']))
+            except:
+                # search for 'list.html'
+                (_, _, path_ending) = path.rpartition('/')
+                if path_ending.endswith('.html'):
+                    return render_to_response(path_ending)
+                else
+                    return render_to_response(''.join([path_ending, '.html']))
+
+        except TemplateDoesNotExist:
+            raise Http404
+
         except:
             if settings.DEBUG:
                 raise
